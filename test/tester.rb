b5e8c86cf2bd146b3e82c2929557ace9094c4270
emlai <emil@cs.helsinki.fi>
2017-02-20 01:03:48 +0200

    refactor out verbosity in test script

--- test/tester.rb
+++ test/tester.rb
@@ -14,15 +14,21 @@ class String
         return $?.success?
     end
 
-    def should_match(input)
-        if not match? input
-            report_error(input)
+    def should_match(*inputs)
+        for input in inputs
+            p input
+            if not match? input
+                report_error(input)
+            end
         end
     end
 
-    def should_not_match(input)
-        if match? input
-            report_error(input)
+    def should_not_match(*inputs)
+        for input in inputs
+            p input
+            if match? input
+                report_error(input)
+            end
         end
     end
 
@@ -41,176 +47,86 @@ end
 
 check "abc" do |p|
     p.should_match "abc"
-
-    p.should_not_match "ab"
-    p.should_not_match "abcc"
+    p.should_not_match "ab", "abcc"
 end
 
 check "a*" do |p|
-    p.should_match ""
-    p.should_match "a"
-    p.should_match "aa"
-    p.should_match "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-
-    p.should_not_match "b"
-    p.should_not_match "aba"
-    p.should_not_match "aaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    p.should_match "", "a", "aa", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    p.should_not_match "b", "aba", "aaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaaaaaaaa"
 end
 
 check "ab*c" do |p|
-    p.should_match "ac"
-    p.should_match "abc"
-    p.should_match "abbc"
-    p.should_match "abbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbc"
-
-    p.should_not_match "ababc"
-    p.should_not_match "aabc"
-    p.should_not_match "abcccccccccccc"
-    p.should_not_match "ab"
+    p.should_match "ac", "abc", "abbc", "abbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbc"
+    p.should_not_match "ababc", "aabc", "abcccccccccccc", "ab"
 end
 
 check "a*a" do |p|
-    p.should_match "a"
-    p.should_match "aa"
-    p.should_match "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-
-    p.should_not_match ""
-    p.should_not_match "aba"
+    p.should_match "a", "aa", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    p.should_not_match "", "aba"
 end
 
 check "a?" do |p|
-    p.should_match ""
-    p.should_match "a"
-
+    p.should_match "", "a"
     p.should_not_match "aa"
 end
 
 check "aa?b?" do |p|
-    p.should_match "a"
-    p.should_match "aa"
-    p.should_match "ab"
-    p.should_match "aab"
+    p.should_match "a", "aa", "ab", "aab"
 end
 
 check "(ab)*" do |p|
-    p.should_match ""
-    p.should_match "ab"
-    p.should_match "abab"
-
-    p.should_not_match "a"
-    p.should_not_match "aa"
-    p.should_not_match "abb"
-    p.should_not_match "aba"
+    p.should_match "", "ab", "abab"
+    p.should_not_match "a", "aa", "abb", "aba"
 end
 
 check "(ab)?" do |p|
-    p.should_match ""
-    p.should_match "ab"
-
-    p.should_not_match "abab"
-    p.should_not_match "a"
-    p.should_not_match "aa"
-    p.should_not_match "abb"
-    p.should_not_match "aba"
+    p.should_match "", "ab"
+    p.should_not_match "abab", "a", "aa", "abb", "aba"
 end
 
 check "abc(def)" do |p|
     p.should_match "abcdef"
-
-    p.should_not_match "abc"
-    p.should_not_match "def"
-    p.should_not_match "abcd"
-    p.should_not_match "cdef"
-    p.should_not_match "abcf"
+    p.should_not_match "abc", "def", "abcd", "cdef", "abcf"
 end
 
 check "a.b" do |p|
-    p.should_match "acb"
-    p.should_match "aab"
-    p.should_match "a b"
-    p.should_match "a.b"
-    p.should_match "a\\b"
-
-    p.should_not_match "ab"
-    p.should_not_match "a\\\\b"
+    p.should_match "acb", "aab", "a b", "a.b", "a\\b"
+    p.should_not_match "ab", "a\\\\b"
 end
 
 check "a\\.b" do |p|
     p.should_match "a.b"
-
-    p.should_not_match "acb"
-    p.should_not_match "aab"
-    p.should_not_match "a b"
-    p.should_not_match "a\\b"
-    p.should_not_match "ab"
-    p.should_not_match "a\\\\b"
+    p.should_not_match "acb", "aab", "a b", "a\\b",  "ab",  "a\\\\b"
 end
 
 check "\\d*" do |p|
-    p.should_match "0123456789"
-    p.should_match ""
-
-    p.should_not_match " "
-    p.should_not_match "d"
-    p.should_not_match "\\d"
-    p.should_not_match "-"
+    p.should_match "0123456789", ""
+    p.should_not_match " ", "d",  "\\d",  "-"
 end
 
 check "\\D\\D?" do |p|
-    p.should_match "a"
-    p.should_match "aa"
-
-    p.should_not_match "0"
-    p.should_not_match "a0"
-    p.should_not_match "0a"
+    p.should_match "a",  "aa"
+    p.should_not_match "0", "a0", "0a"
 end
 
 check "[a-c]*" do |p|
-    p.should_match ""
-    p.should_match "a"
-    p.should_match "b"
-    p.should_match "c"
-    p.should_match "ab"
-    p.should_match "cc"
-    p.should_match "abc"
-
-    p.should_not_match "d"
-    p.should_not_match "ad"
+    p.should_match "", "a", "b", "c", "ab", "cc", "abc"
+    p.should_not_match "d", "ad"
 end
 
 check "[abc]*" do |p|
-    p.should_match ""
-    p.should_match "a"
-    p.should_match "b"
-    p.should_match "c"
-    p.should_match "ab"
-    p.should_match "cc"
-    p.should_match "abc"
-
-    p.should_not_match "d"
-    p.should_not_match "ad"
+    p.should_match "", "a", "b", "c", "ab", "cc", "abc"
+    p.should_not_match "d", "ad"
 end
 
 check "[^bcd]*" do |p|
-    p.should_match "a"
-    p.should_match "e"
-    p.should_match "ae"
-
-    p.should_not_match "b"
-    p.should_not_match "c"
-    p.should_not_match "d"
-    p.should_not_match "ad"
+    p.should_match "a", "e", "ae"
+    p.should_not_match "b", "c", "d", "ad"
 end
 
 check "[^b-d]*" do |p|
-    p.should_match "a"
-    p.should_match "e"
-    p.should_match "ae"
-
-    p.should_not_match "b"
-    p.should_not_match "c"
-    p.should_not_match "d"
-    p.should_not_match "ad"
+    p.should_match "a", "e", "ae"
+    p.should_not_match "b", "c", "d", "ad"
 end
 
 require_relative "errors"
