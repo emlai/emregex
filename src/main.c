8221db147c9a1ba0e892e3cc0d861834b00443cf
emlai <emil@cs.helsinki.fi>
2017-03-10 20:58:40 +0200

    allow --print-ast to be specified anywhere in argv

--- src/main.c
+++ src/main.c
@@ -1,18 +1,39 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 #include "parse.h"
 #include "ast.h"
 #include "match.h"
 
+void printUsage(char** argv) {
+    printf("usage: %s regex-pattern [--print-ast]\n", argv[0]);
+}
+
 int main(int argc, char** argv) {
     if (argc != 2 && argc != 3) {
-        printf("usage: %s regex-pattern [--print-ast]\n", argv[0]);
+        printUsage(argv);
         return 1;
     }
 
-    reParse(argv[1]);
+    char* regexPattern = NULL;
+    bool printAST = false;
 
-    if (argc == 3 && strcmp(argv[2], "--print-ast") == 0) {
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "--print-ast") == 0) {
+            printAST = true;
+        } else {
+            regexPattern = argv[i];
+        }
+    }
+
+    if (regexPattern == NULL) {
+        printUsage(argv);
+        return 1;
+    }
+
+    reParse(regexPattern);
+
+    if (printAST) {
         rePrintAST();
         return 0;
     }
